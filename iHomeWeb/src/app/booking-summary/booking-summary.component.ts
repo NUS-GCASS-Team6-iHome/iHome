e9464c5d3a2d64a3b36b73eda46354d6f78d120d
YuTing1993 <elaine.2028@hotmail.com>
2021-04-26 15:59:50 +0800

    fixed booking summary issue

--- iHomeWeb/src/app/booking-summary/booking-summary.component.ts
+++ iHomeWeb/src/app/booking-summary/booking-summary.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SearchBookingService } from '../service/search-booking.service';
+import { IHomeService, SearchBookingService } from '../service/search-booking.service';
 @Component({
   selector: 'app-booking-summary',
   templateUrl: './booking-summary.component.html',
   styleUrls: ['./booking-summary.component.scss']
 })
 export class BookingSummaryComponent implements OnInit {
-
+  serviceList: IHomeService[];
   constructor(
     private bookingService: SearchBookingService,
     private router: Router) { }
 
   ngOnInit(): void {
     
+  }
+
+  rate(product) {
+
   }
 
 }
