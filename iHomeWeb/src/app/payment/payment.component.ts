b8caaadfe4c187a3e224ec694337d814b15a4dae
dixonlamwc <dixonlam1314@gmail.com>
2021-04-29 21:54:46 +0800

    updated payment frontend

--- iHomeWeb/src/app/payment/payment.component.ts
+++ iHomeWeb/src/app/payment/payment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PaymentService, Payment } from '../service/payment.service';
 import { AccountServiceService } from '../service/account-service.service';
+import { IHomeBooking } from '../service/booking-service.service';
 
 @Component({
   selector: 'app-payment',
@@ -9,7 +10,7 @@ import { AccountServiceService } from '../service/account-service.service';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-  payment : Payment = new Payment('', '', '', 0);
+  payment : Payment = new Payment('', '', '' ,'','', 0);
   accountId = '';
 
   constructor(
@@ -20,10 +21,12 @@ export class PaymentComponent implements OnInit {
 
   ngOnInit(): void {
   this.accountId = this.accountService.getLoginAccount().accountid;
+  this.payment.paymentStatus = 'testing-';
   }
 
   submitPayment(): void {
   console.log(this.payment);
+  this.payment.accountId = this.accountService.getLoginAccount().accountid;
       this.paymentService.createPayment(this.payment)
           .subscribe( data => {
             this.router.navigate(['payment-success'])
